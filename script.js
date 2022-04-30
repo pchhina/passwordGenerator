3a751e3f1d14f0c64e1613eac9e986a9bf70a794
Paramjot Singh <param.chhina@gmail.com>
2022-04-30 11:10:11 -0500

    basic logic implemented

---
+++ script.js
@@ -0,0 +1,32 @@
+let lowerCase = 'abcdefghijklmnopqrstuvqxyz';
+let upperCase = lowerCase.toUpperCase();
+let nums = '0123456789';
+let chars = lowerCase.split('').concat(upperCase.split(''), nums.split(''));
+
+const generateEl = document.getElementById('generate');
+generateEl.addEventListener('click', populatePasswords);
+
+let pass1El = document.getElementById('pass-1');
+let pass2El = document.getElementById('pass-2');
+let pass3El = document.getElementById('pass-3');
+let pass4El = document.getElementById('pass-4');
+let passwordButtons = [pass1El, pass2El, pass3El, pass4El];
+
+function generatePassword() {
+	password = '';
+	for (let i = 0; i < 10; i++) {
+		password += chars[randomInt(0, chars.length - 1)];
+	}
+	return password;
+}
+
+// produces a random integer in range [m, n]
+function randomInt(m, n) {
+	return Math.floor(Math.random() * (n - m + 1)) + m;
+}
+
+function populatePasswords() {
+	for (let el of passwordButtons) {
+		el.textContent = generatePassword();
+	}
+}
